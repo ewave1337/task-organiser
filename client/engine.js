2a6f4588670fb1d02c660f74e16e9211d511f335
vanjuxin-dju <sokol.vdu@gmail.com>
2014-03-14 12:36:42 +0300

    Функции написаны на jquery. Исправлена основная часть кода

--- client/engine.js
+++ client/engine.js
@@ -5,31 +5,51 @@ values = [];
 page = 1;
 
 function currentDateString() {
-	date = new Date();
+	var date = new Date();
 	return "" + (date.getMonth() + 1) + "-" + date.getDate() + "-" + (date.getYear() < 200 ? date.getYear() + 1900 : date.getYear()) + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
 function addElem(id, name, description, date, isComplete) {
-	var inner = "<span>" + id + "</span> <span>" + name + "</span> <span>" + description + "</span>" +
-    "<span>" + date + "</span> <span>" + isComplete + "</span>" +
+	var inner = "<span>" + id + "</span> <span>" + name + "</span> <span>" + description + "</span> " +
+    "<span>" + date + "</span> <span>" + isComplete + "</span> " +
     "<span><input type='button' value='Edit' onclick='updateTask(\"" + id + "\")'>" +
     "<input type='button' value='Remove' onclick='removeTask(\"" + id + "\")'></span>";
 
   $('<div class="items" id="' + id + '" draggable>' + inner + '</div>').insertBefore(document.getElementById('add'))
 }
 
-function addTask() {
-	document.getElementById("add").style.visibility = "visible";
-	document.getElementById("whattodo").innerText = "Добавить";
-	document.getElementById("id_field").value = "el" + (lastKey + 1);
-}
+$(document).ready(function () {
+	$('#newtask').on("click", function () {
+		$('#add').css("visibility", "visible");
+		$('#whattodo').text("Добавить");
+		$('id_field').val("el" + (lastKey + 1));
+	});
 
-function confirm() {
+	$('#confirm').on("click", function () {
+		id = $("#id_field").val();
+		name = $("#name_field").val();
+		description = $("#description_field").val();
+		isComplete = $("#complete_field").prop("checked");
+		if (/*!localStorage[id]*/ true) {
+			date = currentDateString();
+			//jsonString = '{"id":"'+ id +'", "name":"' + name +'","description":"' + description + '","date":"'+date+'","iscomplete":"'+isComplete+'"}';
+			//localStorage.setItem(id, jsonString);
+			addElem(id, name, description, date, isComplete);
+			hide();
+			length++;
+			lastKey++;
+		} 
+	});
+
+	$('#hide').on("click", hide());
+});
+
+/*function confirm() {
 	id = document.getElementById("id_field").value;
 	name = document.getElementById("name_field").value;
 	description = document.getElementById("description_field").value;
 	isComplete = document.getElementById("complete_field").checked;
-	if (!localStorage[id]) {
+	if (/*!localStorage[id]*/ /*true) {
 		date = currentDateString();
 		//jsonString = '{"id":"'+ id +'", "name":"' + name +'","description":"' + description + '","date":"'+date+'","iscomplete":"'+isComplete+'"}';
 		//localStorage.setItem(id, jsonString);
@@ -42,35 +62,34 @@ function confirm() {
 			//jsonString = '{"id":"'+ id +'","name":"' + name +'","description":"' + description + '","date":"'+document.getElementById("date_field").value +'","iscomplete":"'+isComplete+'"}';
 			//localStorage.setItem(id, jsonString);
 			el = document.getElementById(id);
-			el.innerHTML = "<li>" + id + "</li> <li>" + name + "</li> <li>" + description + "</li> <li>" + document.getElementById("date_field").value + "</li> <li>" + isComplete + "</li> <li><input type='button' value='Edit' onclick='updateTask(\"" + id + "\")'><input type='button' value='Remove' onclick='removeTask(\"" + id + "\")'></li>";
+			el.innerHTML = "<span>" + id + "</span> <span>" + name + "</span> <span>" + description + "</span> <span>" + document.getElementById("date_field").value + "</span> <span>" + isComplete + "</span> <span><input type='button' value='Edit' onclick='updateTask(\"" + id + "\")'><input type='button' value='Remove' onclick='removeTask(\"" + id + "\")'></span>";
 			hide();
 		} else
 			alert("Нельзя создать новый объект с существующим идентификатором!");
 	}
-}
+}*/
 
 function hide() {
-	document.getElementById("id_field").value = "";
-	document.getElementById("name_field").value = "";
-	document.getElementById("description_field").value = "";
-	document.getElementById("complete_field").checked = false;
-	document.getElementById('add').style.visibility = 'hidden';
+	$("#id_field").val("");
+	$("#name_field").val("");
+	$("#description_field").val();
+	$("#complete_field").prop("checked", false);
+	$('#add').css("visibility", "hidden");
 }
 
 function removeTask(id) {
-	el = document.getElementById(id);
-	el.parentNode.removeChild(el);
 	//localStorage.removeItem(id);
+	$("#" + id).remove();
 	length--;
 }
 
 function updateTask(id) {
-	document.getElementById("add").style.visibility = "visible";
-	document.getElementById("whattodo").innerText = "Изменить";
-	document.getElementById("id_field").value = id;
+	$('#add').css("visibility", "visible");
+	$("#whattodo").text("Изменить");
+	$("#id_field").val(id);
 	//elem = JSON.parse(localStorage.getItem(id));
-	document.getElementById("name_field").value = elem.name;
-	document.getElementById("description_field").value = elem.description;
-	document.getElementById("date_field").value = elem.date;
-	document.getElementById("complete_field").checked = (elem.iscomplete == "true" ? true : false);
+	$("#name_field").val(elem.name);
+	$("#description_field").val(elem.description);
+	$("#date_field").val(elem.date);
+	$("#complete_field").prop("checked", (elem.iscomplete == "true" ? true : false);
 }
